937b48cab0dc8422f59f96aff88ec7746451c301
Kirill Tepljakov <super.css2017@yandex.ru>
2021-12-17 15:21:09 +0300

    доверстал баннер

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-const showТumber = document.querySelector('.contact__phone-button');
+const btnShowТumber = document.querySelector('.contact__phone-button');
 const phoneNumber = document.querySelector('.contact__phone-number');
 
-showТumber.addEventListener('click', () => {
+btnShowТumber.addEventListener('click', () => {
     if(phoneNumber.textContent === '+7(911)554-...') {
         phoneNumber.textContent = '+7(911)554-12-76';
-        showТumber.textContent = 'скрыть';
+        btnShowТumber.textContent = 'скрыть';
     } else {
         phoneNumber.textContent = '+7(911)554-...';
-        showТumber.textContent = 'показать номер';
+        btnShowТumber.textContent = 'показать номер';
     }
     
 })
